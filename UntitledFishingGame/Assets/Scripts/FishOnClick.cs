eb7c8f6eff6de65db22fc79776021511a40be7f7
noatakak <noatak.post@gmail.com>
2022-10-04 21:47:41 -0700

    Started script for rod casting

---
+++ UntitledFishingGame/Assets/Scripts/FishOnClick.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FishOnClick : MonoBehaviour
+{
+    Vector2 mouseStart;
+    Vector2 mouseEnd;
+
+    public bool click;
+    bool controllerClick;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
