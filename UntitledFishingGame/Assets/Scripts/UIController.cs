af2ce065861db8ecd008978ec0712c94573db8c5
noatakak <noatak.post@gmail.com>
2022-10-05 21:35:39 -0700

    Worked on Menu

--- UntitledFishingGame/Assets/Scripts/UIController.cs
+++ UntitledFishingGame/Assets/Scripts/UIController.cs
@@ -5,6 +5,9 @@ using UnityEngine.SceneManagement;
 
 public class UIController : MonoBehaviour
 {
+    public GameObject helpPanel;
+    public int slideTime;
+    bool slide;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,11 +17,35 @@ public class UIController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (slide)
+        {
+            StartCoroutine(slideUpUI(helpPanel, slideTime));
+        }
     }
 
     public void Open(string path)
     {
         SceneManager.LoadScene(path);
+    }
+
+    public void startSlide()
+    {
+        slide = true;
+    }
+
+    IEnumerator slideUpUI(GameObject slidingUI, int time)
+    {
+        float i = 0;
+        float rate = .7f / time;
+
+        Vector3 toPos = new Vector3(0, 0, 0);
+        Vector3 fromPos = new Vector3(0, -600, 0);
+        while (i < 1)
+        {
+            i += Time.deltaTime * rate;
+            slidingUI.transform.localPosition = Vector3.Lerp(fromPos, toPos, i);
+            yield return 0;
+        }
+        slide = false;
     }
 }
